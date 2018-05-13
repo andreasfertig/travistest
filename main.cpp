65a2f83ea479b455e3eb15538fd0886fd372f7c7
Andreas Fertig <mail@andyf.de>
2018-05-13 12:25:11 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+
+template<typename... Args>
+inline void Printf(const char* fmt, Args&&... args)
+{
+    if constexpr(0 < (sizeof...(args))) {
+        printf(fmt, args...);
+    } else {
+        printf("%s", fmt);
+    }
+}
+//-----------------------------------------------------------------------------
+
+
+int main()
+{
+    Printf("Hello", " ", "World");
+}
