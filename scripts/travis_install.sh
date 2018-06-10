238781b8dbe5dad7dcdd8342bccc5aead2c0d79b
Andreas Fertig <mail@andyf.de>
2018-06-10 14:22:12 +0200

    upload

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
+   brew update &> /dev/null
    brew install cmake || brew upgrade cmake
    brew install xz || brew upgrade xz
 
@@ -8,7 +9,7 @@ if [[ "${TRAVIS_OS_NAME}" == "osx" ]]; then
    cd ${TRAVIS_BUILD_DIR}/clang
    wget https://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
 
-   xz -vd clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
+   xz -d clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz
    tar -xf clang+llvm-6.0.0-x86_64-apple-darwin.tar
    mv clang+llvm-6.0.0-x86_64-apple-darwin current
 
